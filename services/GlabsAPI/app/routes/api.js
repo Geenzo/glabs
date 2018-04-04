4e45fcf9c1a6176f1b9cba21e7f9c82abfca4576
Darren <darrenhay1994@gmail.com>
2018-04-04 02:08:59 +0100

    updating /v1/ReturnedDebitItems response to have total amount in response

--- services/GlabsAPI/app/routes/api.js
+++ services/GlabsAPI/app/routes/api.js
@@ -5,8 +5,12 @@ module.exports = (app) => {
   app.route('/v1/ReturnedDebitItems')
     .get((req, res) => {  
       models.BacsDocument.find({state: "Processed"}).then((returnDebits) => {
+        let responseBody = {
+          returnDebits: returnDebits,
+          totalProcessedBacs: returnDebits.length
+        }
         console.log(`Request for "/v1/ReturnedDebitItems" received - 200 response`);
-        res.status(200).send({returnDebits});
+        res.status(200).send(responseBody);
       }, (err) => {
         console.log(`Request for "/v1/ReturnedDebitItems" received - 200 response`);
         res.status(400).send(err);
