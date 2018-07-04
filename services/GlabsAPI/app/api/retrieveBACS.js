5608a7a2c35c2dc827571e9f19289e2078965c3d
Darren Hay <darrenhay1994@gmail.com>
2018-07-04 21:49:20 +0100

    refactoring retrieve bacs to use AWS S3 buckets as storage location

--- services/GlabsAPI/app/api/retrieveBACS.js
+++ services/GlabsAPI/app/api/retrieveBACS.js
@@ -9,10 +9,6 @@ const fs = require('fs'),
 
 let {BUCKET_NAME, BUCKET_NAME_NEW_BACS, BUCKET_NAME_ARCHIVED_BACS, IAM_USER_KEY, IAM_USER_SECRET} = require('../../../../secrets.js')
 
-console.log(BUCKET_NAME_NEW_BACS)
-console.log(IAM_USER_KEY)
-console.log(IAM_USER_SECRET)
-
 let s3bucket = new AWS.S3({
     accessKeyId: IAM_USER_KEY,
     secretAccessKey: IAM_USER_SECRET,
@@ -52,7 +48,7 @@ const retrieveBacsFromAWSS3Bucket = () => {
     })
 
   })
-};
+}
 
 const readBacFiles = (retrievedBacs) => {   
     let bacs = retrievedBacs.map(bacfile => {
@@ -68,7 +64,7 @@ const readBacFiles = (retrievedBacs) => {
     })
 
     return Promise.all(bacs)
-};
+}
 
 const parseBacFile = (bacFilesXMLData) => {
     let parsedBacsData = bacFilesXMLData.map((bacXMLData) => {
@@ -83,22 +79,20 @@ const parseBacFile = (bacFilesXMLData) => {
 }
 
 const saveBacToDB = (parsedBacData) => {
-    
     let savedBac = parsedBacData.map(parsedBac => {
-
         return new Promise((resolve, reject) => {
             let bacsDocument = new BacsDoc({
-              name: `BAC - ${parsedBac.BACSDocument.Data.ARUDD.Header.adviceNumber} - ${parsedBac.BACSDocument.Data.ARUDD.Header.currentProcessingDate} - ${parsedBac.BACSDocument.Data.ARUDD.AddresseeInformation.name}`,
-              created: moment().format('DD-MM-YYYY'),
-              updated: moment().format('DD-MM-YYYY'),
-              bacsDocument: parsedBac.BACSDocument,
-              state: "Ready For Processing"
+                name: `BAC - ${parsedBac.BACSDocument.Data.ARUDD.Header.adviceNumber} - ${parsedBac.BACSDocument.Data.ARUDD.Header.currentProcessingDate} - ${parsedBac.BACSDocument.Data.ARUDD.AddresseeInformation.name}`,
+                created: moment().format('DD-MM-YYYY'),
+                updated: moment().format('DD-MM-YYYY'),
+                bacsDocument: parsedBac.BACSDocument,
+                state: "Ready For Processing"
             })
             
-            return bacsDocument.save().then(savedBac => {
-                resolve(savedBac)
+            return bacsDocument.save().then(bac => {
+                resolve(bac)
             })
-          })
+        })
     })
     
     return Promise.all(savedBac)
@@ -106,64 +100,125 @@ const saveBacToDB = (parsedBacData) => {
 
 const archiveProcessedBac = (savedBac) => {
     return new Promise((resolve, reject) => {
-        const DirectoryDate = moment().subtract(1, 'days').format('DD-MM-YYYY');
-        let newDir = path.join(`BACSDirectory/newBACS/${DirectoryDate}`)
+        return retrieveBacsFromAWSS3Bucket()
+            .then(retrievedBacs => {
+                let bacs = retrievedBacs.map(bacfile => {
+                    return new Promise ((resolve, reject) => {
+                        let params = {Bucket: BUCKET_NAME, Key: bacfile.Key}
+            
+                        s3bucket.getObject(params, function(err, json_data) {
+                            if (!err) {
+                                resolve(json_data.Body)
+                            }
+                       })
+                    })
+                })
+            
+                return Promise.all(bacs)
+            })
+            .then(result => {
+                return new Promise((resolve, reject) => {
+                    const DirectoryDate = moment().subtract(1, 'days').format('DD-MM-YYYY');
+                    const directoryLocation = `BACSDirectory/archivedBACS/${DirectoryDate}-BACS.zip`
+                    var output = fs.createWriteStream(directoryLocation)
+            
+                    var archive = archiver('zip', {
+                    zlib: { level: 9 } // Sets the compression level.
+                    })
+            
+                    output.on('close', function() {
+
+                            fs.readFile(directoryLocation, function read(err, data) {
+                                if (err) {
+                                    throw err
+                                }
+
+                                let achriveKey = `${DirectoryDate}.zip`
+                                s3bucket.createBucket(function () {
+                                    var params = {
+                                    Bucket: BUCKET_NAME_ARCHIVED_BACS,
+                                    Key: achriveKey,
+                                    Body: data
+                                    };
+                                    s3bucket.upload(params, function (err, data) {
+                                    if (err) {
+                                        console.log('error in callback')
+                                        reject(err)
+                                    }
+                                    console.log('success')
+                                    console.log(data)
+                                    
+                                    resolve(savedBac)
+                                    })
+                                })
+                            })
+
         
-        var output = fs.createWriteStream(`BACSDirectory/archivedBACS/${DirectoryDate}-BACS.zip`);
 
-        var archive = archiver('zip', {
-        zlib: { level: 9 } // Sets the compression level.
-        });
-
-        output.on('close', function() {
-        console.log("Yesterdays BACS have been archived.")   
-        console.log(archive.pointer() + ' total bytes');
-        resolve(savedBac)
-        });
-
-        output.on('end', function() {
-        console.log('Data has been drained');
-        });
-
-        // good practice to catch warnings (ie stat failures and other non-blocking errors)
-        archive.on('warning', function(err) {
-        if (err.code === 'ENOENT') {
-            // log warning
-        } else {
-            // throw error
-            throw err;
-        }
-        });
-
-        // good practice to catch this error explicitly
-        archive.on('error', function(err) {
-        throw err;
-        });
-
-        archive.pipe(output);
-        
-        archive.directory(newDir, false).finalize();
+                    })
+            
+                    output.on('end', function() {
+                    console.log('Data has been drained')
+                    })
+            
+                    // good practice to catch warnings (ie stat failures and other non-blocking errors)
+                    archive.on('warning', function(err) {
+                    if (err.code === 'ENOENT') {
+                        // log warning
+                    } else {
+                        // throw error
+                        throw err;
+                    }
+                    })
+            
+                    // good practice to catch this error explicitly
+                    archive.on('error', function(err) {
+                    throw err;
+                    })
+            
+                    archive.pipe(output)
+                    
+                    archive.finalize()
+                })
+            })
+            .then(savedBac => {
+                console.log('this is saved')
+                return resolve(savedBac)
+            })
                 
-    });
+    })
 }
 
 const deleteArchivedBacsFromNewBACFolder = (savedBacs) => {
-    const DirectoryDate = moment().subtract(1, 'days').format('DD-MM-YYYY')
-    let newDir = path.join(`BACSDirectory/newBACS/${DirectoryDate}`)
-    let numberOfFilesRemoved
-    
-    fs.readdir(newDir, (err, files) => {
-        if (err) throw err
-        numberOfFilesRemoved = files.length
-        console.log(`${numberOfFilesRemoved} BACS have been removed from the new BACs directory`)
-        for (const file of files) {
-          fs.unlink(path.join(newDir, file), err => {
-            if (err) throw err;
-          });
+    return new Promise((resolve, reject) => {
+        let params = { 
+            Bucket: BUCKET_NAME,
+            Delimiter: '',
+            Prefix: 'newBacs/' 
         }
-      });
+        let folderKey = 'newBacs'
 
-    return Promise.resolve(savedBacs)
+        s3bucket.listObjects(params, function(err, data) {
+          if (err) {
+            console.log('There was an error deleting your album: ', err.message)
+          }
+          let oldBacs = data.Contents.filter(bac => bac.Size > 0)
+          let objects = oldBacs.map(function(object) {
+            return {Key: object.Key}
+          })
+          s3bucket.deleteObjects({
+            Bucket: BUCKET_NAME,
+            Delete: {Objects: objects, Quiet: true}
+          }, function(err, data) {
+            if (err) {
+                console.log('There was an error deleting your album: ', err.message)
+            }
+            console.log('Successfully deleted album.')  
+            resolve(oldBacs)
+          })
+        })
+
+    })
     
 }
 
@@ -179,7 +234,7 @@ const RetrieveBacsDocs = () => {
             return savedBacs.length
         })
         .catch((err) => {
-            console.log(`${err} - RetrieveBacsDocs task now exiting...`);
+            console.log(`${err} - RetrieveBacsDocs task now exiting...`)
         })
 }
 
